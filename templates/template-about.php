b2654e6cb7b142a2995b97a1a51713ab8cf640d0
Firstname Lastname <andres.c@icanlocalize.com>
2016-07-20 20:48:04 +0200

    Custom Fields: Template About us and Quality

--- templates/template-about.php
+++ templates/template-about.php
@@ -8,138 +8,94 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<section class="section-hero about-hero">
-				<div class="section-hero__content">
-					<h2 class="section-hero__title">About us</h2>
-				</div>
-			</section><!-- .about-hero -->
-			<section class="home-description">
-				<p class="">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh, ultricies in nisl eget, commodo tempor lorem. Mauris enim mi, ornare non euismod vitae, pretium eget lorem. 
-				</p>
-			</section><!-- .home-description -->
-			<section class="about-vision">
-				<div class="about-vision__item-content">
-					<h3 class="about-vision__item-title">our vision, mission, and values</h3>
-					<p class="about-vision__item-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vehicula purus vitae erat gravida, quis efficitur nunc consectetur. Etiam vulputate leo sit amet lectus pellentesque pharetra. Cras et nisl at enim gravida aliquam. Aenean id ante gravida, rhoncus odio sagittis, aliquet lacus. Cras in massa elementum turpis accumsan feugiat nec id purus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed convallis sit amet justo id tempor. Nam placerat, justo quis tempus suscipit, sem libero viverra mi, at sodales neque lacus et nibh. Donec in interdum arcu. Vestibulum quis magna hendrerit, volutpat enim eu, feugiat odio. Vivamus lectus enim, tempus suscipit elementum in, consectetur at dui. Aliquam erat volutpat.
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<section class="section-hero about-hero">
+					<div class="section-hero__content">
+						<?php the_content(); ?>
+					</div>
+				</section><!-- .about-hero -->
+				<section class="home-description">
+					<p class="">
+						<?php echo types_render_field( "section-about-us-introduction", array( ) ); ?>
 					</p>
-			</div>
-			</section><!-- .about-ingredients -->
-
-			<section class="section-banner about-banner" style="background-image:url('<?php bloginfo('template_directory'); ?>/img/bg-question-banner.jpg');" data-100-top="background-position:0px 0px;" data-1000-top="background-position: 0px 50px;">
-				<h3 class="section-banner__title">Frequently Asked Questions</h3>
-			</section><!-- .about-nutrition -->
-
-			<section class="about-questions"><!--
-
-			--><div class="about-thequestion__wrapper"><!--
-				 --><div class="about-thequestion__item title_dotted "></div><!--
-				 --><div class="about-thequestion__answer title_dotted"></div><!--
-			--></div>
-			 	<div class="about-thequestion__wrapper"><!--
-				
-				 --><div class="about-thequestion__item">
-						<span class="about-thequestion__number">1</span><!--
-						--><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit? </p>
-					</div><!--
-				
-				 --><div class="about-thequestion__answer">
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh, ultricies in nisl eget, commodo tempor. Lorem ipsum dolor sit amet.
+				</section><!-- .home-description -->
+				<section class="about-vision">
+					<div class="about-vision__item-content">
+						<h3 class="about-vision__item-title"><?php echo types_render_field( "section-vision-title", array("output" => "raw") ); ?></h3>
+						<p class="about-vision__item-description"><?php echo types_render_field( "section-vison-content", array( ) ); ?>
 						</p>
-					</div><!--
-			--></div>
-
-				<div class="about-thequestion__wrapper">
-					<div class="about-thequestion__item">
-						<span class="about-thequestion__number">2</span><!--
-						--><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit? </p>
-					</div><!--
-				
-				--><div class="about-thequestion__answer">
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh, ultricies in nisl eget, commodo tempor. Lorem ipsum dolor sit amet.
-						</p>
-					</div>
 				</div>
+				</section><!-- .about-ingredients -->
 
-				<div class="about-thequestion__wrapper">
-					<div class="about-thequestion__item">
-						<span class="about-thequestion__number">3</span><!--
-						--><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit? </p>
-					</div><!--
-				
-				--><div class="about-thequestion__answer">
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh, ultricies in nisl eget, commodo tempor. Lorem ipsum dolor sit amet.
-						</p>
-					</div>
-				</div>	
-				<div class="about-thequestion__wrapper">
-					<div class="about-thequestion__item">
-						<span class="about-thequestion__number">4</span><!--
-						--><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit? </p>
-					</div><!--
-				
-				--><div class="about-thequestion__answer">
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh, ultricies in nisl eget, commodo tempor. Lorem ipsum dolor sit amet.
-						</p>
-					</div>
-				</div>	
-				<div class="about-thequestion__wrapper">
-					<div class="about-thequestion__item">
-						<span class="about-thequestion__number">5</span><!--
-						--><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit? </p>
-					</div><!--
-				
-				--><div class="about-thequestion__answer">
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh, ultricies in nisl eget, commodo tempor. Lorem ipsum dolor sit amet.
-						</p>
-					</div>
-				</div>					
-				<div class="about-thequestion__wrapper">
-					<div class="about-thequestion__item">
-						<span class="about-thequestion__number">2</span><!--
-						--><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit? </p>
-					</div><!--
-				
-				--><div class="about-thequestion__answer">
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh, ultricies in nisl eget, commodo tempor. Lorem ipsum dolor sit amet.
-						</p>
-					</div>
-				</div>
-				<div class="about-thequestion__wrapper"><!--
-				 --><div class="about-thequestion__item title_dotted-bas "></div><!--
-				 --><div class="about-thequestion__answer title_dotted-bas"></div><!--
-			--></div>
+				<section class="section-banner about-banner" style="background-image:url('<?php bloginfo('template_directory'); ?>/img/bg-question-banner.jpg');" data-100-top="background-position:0px 0px;" data-1000-top="background-position: 0px 50px;">
+					<h3 class="section-banner__title">Frequently Asked Questions</h3>
+				</section><!-- .about-nutrition -->
 
-			</section><!-- .about-questions -->	
+				<section class="about-questions"><!--
+				--><div class="about-thequestion__wrapper"><!--
+					 --><div class="about-thequestion__item title_dotted "></div><!--
+					 --><div class="about-thequestion__answer title_dotted"></div><!--
+				--></div>
+					<?php
+						$args = array(
+							'post_type' 		=> 'faq', 	//Costum type child of FAQs			
+							'order'				=> 'ASC',		// List in ascending order
+							'orderby'      		=> 'id',		// List them in their menu order
+						);
+						$i = 1;
+						$QueryQA = new WP_Query($args);
+					?>
+					<?php while ($QueryQA->have_posts()) : $QueryQA->the_post(); ?>		
+		
+				 	<div class="about-thequestion__wrapper"><!--
+					
+					 --><div class="about-thequestion__item">
+							<span class="about-thequestion__number"><?php echo $i;?></span><!--
+						
+						--><div class="about-thequestion__number--div"><?php the_title(); ?></div>
+						</div><!--
+					
+					 --><div class="about-thequestion__answer">
+							<?php the_content(); ?>
+							<?php $i++; //counter?>						
+						</div><!-- End of about-thequestion__wrapper
+				 --></div>
+					<?php endwhile; // End of the loop. ?>
 
-			<section class="section-banner about-banner" style="background-image:url('<?php bloginfo('template_directory'); ?>/img/bg-question-banner.jpg');" data-100-top="background-position:0px 0px;" data-1000-top="background-position: 0px 50px;">
-				<h3 class="section-banner__title
-				">Contact Us</h3>
-			</section><!-- .about-questions -->
-			<section class="about-contact">
-				<div class="about-contact__item-content ">
-					<p class="about-contact__text title_dotted">
-						For questions and product inquiries, please complete and submit the form or send us an email to get connected!
-					</p>
-					<p class="about-contact__adresses">
-						<span class="about-contact__title">COMPANY LOCATION</span><br/>
-						Santa Monica, CA 90410
-					</p>
-					<p class="about-contact__adresses title_dotted-bas">
-						<span class="about-contact__title">EMAIL ADDRESS</span><br/>
-						<a href="mailto:info@volupta.com">info@volupta.com</a>
-					</p>
-				</div><!--
-				--><div class="about-contact__form">
-					    <?php echo do_shortcode('[contact-form-7 id="41" title="Contact form 1"]'); ?>
-				</div>
-			</section>
+					<div class="about-thequestion__wrapper"><!--
+					 --><div class="about-thequestion__item title_dotted-bas "></div><!--
+					 --><div class="about-thequestion__answer title_dotted-bas"></div><!--
+				--></div>
+
+				</section><!-- .about-questions -->	
+
+				<?php wp_reset_postdata(); ?>
+
+				<section class="section-banner about-banner" style="background-image:url('<?php echo types_render_field( "section-contact-image", array( "alt" => "", "url" => "true" ) ); ?>');" data-100-top="background-position:0px 0px;" data-1000-top="background-position: 0px 50px;">
+					<h3 class="section-banner__title
+					"><?php echo types_render_field( "section-contact-title", array( ) ); ?></h3>
+				</section><!-- .about-questions -->
+				<section class="about-contact">
+					<div class="about-contact__item-content ">
+						<p class="about-contact__text title_dotted">
+							<?php echo types_render_field( "section-contact-content", array( ) ); ?>
+						</p>
+						<p class="about-contact__adresses">
+							<span class="about-contact__title"><?php echo types_render_field( "section-contact-adress-title", array( ) ); ?></span><br/>
+							<?php echo types_render_field( "section-contact-adress-content", array( ) ); ?>
+						</p>
+						<p class="about-contact__adresses title_dotted-bas">
+							<span class="about-contact__title"><?php echo types_render_field( "section-email-title", array( ) ); ?></span><br/>
+							<a href="mailto:info@volupta.com"><?php echo types_render_field( "section-email-adress", array( ) ); ?></a>
+						</p>
+					</div><!--
+					--><div class="about-contact__form">
+						    <?php echo do_shortcode('[contact-form-7 id="41" title="Contact form 1"]'); ?>
+					</div>
+				</section>
+
+			<?php endwhile; // End of the loop.?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
--- templates/template-quality.php
+++ templates/template-quality.php
@@ -8,84 +8,86 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<section class="section-hero quality-hero">
-				<div class="section-hero__content quality-hero__content">
-					<h2 class="section-hero__title">Quality you can trust</h2>
-					<p class="section-hero__sub-title">Lorem ipsum dolor sit, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh, ultricies in nisl eget, tempor lorem.</p>
+			<?php while ( have_posts() ) : the_post(); ?>
 
-				</div>
-			</section><!-- .quality-hero -->
-			<section class="home-description">
-				<p class="">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh, ultricies in nisl eget, commodo tempor lorem. Mauris enim mi, ornare non euismod vitae, pretium eget lorem. 
-				</p>
-			</section><!-- .home-description -->
-			<section class="quality">
-				<div class="quality-description__wrapper">
-					<div class="quality-description__image">
-						<img src="<?php bloginfo('template_directory'); ?>/img/quality-pic.jpg" alt="">
-					</div><!--
-					
-				--><div class="quality-description__content">
-						<h3 class="quality__title title_dotted">At Origin - Our Sources</h3>
-						<p class="quality__description">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh, ultricies in nisl eget, commodo tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh, ultricies in nisl eget, commodo tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh.
-						</p>
+				<section class="section-hero quality-hero">
+					<div class="section-hero__content quality-hero__content">
+						<?php the_content(); ?>
 					</div>
-				</div>
-				<div class="quality-description__wrapper">
-					<div class="quality-description__image">
-						<img src="<?php bloginfo('template_directory'); ?>/img/quality-pic.jpg" alt="">
-					</div><!--
-					
-				--><div class="quality-description__content">
-						<h3 class="quality__title title_dotted">At the packing facility</h3>
-						<p class="quality__description">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh, ultricies in nisl eget, commodo tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh, ultricies in nisl eget, commodo tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh.
-						</p>
+				</section><!-- .quality-hero -->
+				<section class="home-description">
+					<p class="">
+						<?php echo types_render_field( "section-quality-introduction", array( "output" => "raw") ); ?>
+					</p>
+				</section><!-- .home-description -->
+				<section class="quality">
+					<div class="quality-description__wrapper">
+						<div class="quality-description__image">
+							<?php echo types_render_field( "section-sources-image", array( "alt" => "", "proportional" => "true" ) ); ?>
+						</div><!--
+						
+					--><div class="quality-description__content">
+							<h3 class="quality__title title_dotted"><?php echo types_render_field( "section-sources-title", array( ) ); ?></h3>
+							<p class="quality__description">
+							<?php echo types_render_field( "section-sources-content", array("output" => "raw" ) ); ?>
+							</p>
+						</div>
 					</div>
-				</div>
-			</section><!-- .quality-description -->
+					<div class="quality-description__wrapper">
+						<div class="quality-description__image">
+							<?php echo types_render_field( "section-packing-image", array( "alt" => "", "proportional" => "true" ) ); ?>
+						</div><!--
+						
+					--><div class="quality-description__content">
+							<h3 class="quality__title title_dotted"><?php echo types_render_field( "section-packing-title", array( ) ); ?></h3>
+							<p class="quality__description">
+							<?php echo types_render_field( "section-packing-content", array( "output" => "raw") ); ?>
+							</p>
+						</div>
+					</div>
+				</section><!-- .quality-description -->
 
-			<section class="section-banner quality-banner" style="background-image:url('<?php bloginfo('template_directory'); ?>/img/bg-quality-banner.jpg');" data-100-top="background-position:0px 0px;" data-1000-top="background-position: 0px 50px;">
-				<h3 class="section-banner__title">Certifications</h3>
-			</section><!-- .quality-questions -->
+				<section class="section-banner quality-banner" style="background-image:url('<?php echo types_render_field( "section-certif-image", array( "alt" => "", "url" => "true" ) ); ?>');" data-100-top="background-position:0px 0px;" data-1000-top="background-position: 0px 50px;">
+					<h3 class="section-banner__title"><?php echo types_render_field( "section-certif-title", array( ) ); ?></h3>
+				</section><!-- .quality-questions -->
 
-			<section class="quality-certifications">
-				<div class="quality-certifications__wrapper">
-					<div class="quality-certifications__pictitle title_dotted">
-						<img src="<?php bloginfo('template_directory'); ?>/img/usda-organic.png" alt=""><!--
-						--><h3 class="quality__title">Certified USDA Organic</h3>
+				<section class="quality-certifications">
+					<div class="quality-certifications__wrapper">
+						<div class="quality-certifications__pictitle title_dotted">
+							<?php echo types_render_field( "section-certif-usda-image", array( "alt" => "", "proportional" => "true" ) ); ?><!--
+							--><h3 class="quality__title"><?php echo types_render_field( "section-certif-usda-title", array( ) ); ?></h3>
+						</div><!--
+					--><div class="quality-certifications__content">
+							<p class="quality__certifications">
+							<?php echo types_render_field( "section-certif-usda-content", array("output" => "raw" ) ); ?>
+							</p>
+						</div>
 					</div><!--
-				--><div class="quality-certifications__content">
-						<p class="quality__certifications">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh, ultricies in nisl eget, commodo tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh, ultricies in nisl eget, commodo tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh.
-						</p>
-					</div>
-				</div><!--
-			--><div class="quality-certifications__wrapper">
-					<div class="quality-certifications__pictitle title_dotted">
-						<img src="<?php bloginfo('template_directory'); ?>/img/non-gmo.jpg" alt=""><!--
-						--><h3 class="quality__title">Verified non GMO</h3>
+				--><div class="quality-certifications__wrapper">
+						<div class="quality-certifications__pictitle title_dotted">
+							<?php echo types_render_field( "section-certif-nongmo-image", array( "alt" => "", "proportional" => "true" ) ); ?><!--
+							--><h3 class="quality__title"><?php echo types_render_field( "section-certif-nongmo-title", array( ) ); ?></h3>
+						</div><!--
+					--><div class="quality-certifications__content">
+							<p class="quality__certifications">
+							<?php echo types_render_field( "section-certif-nongmo-content", array("output" => "raw" ) ); ?>
+							</p>
+						</div>
 					</div><!--
-				--><div class="quality-certifications__content">
-						<p class="quality__certifications">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh, ultricies in nisl eget, commodo tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh, ultricies in nisl eget, commodo tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh.
-						</p>
+				--><div class="quality-certifications__wrapper">
+						<div class="quality-certifications__pictitle title_dotted">
+							<?php echo types_render_field( "section-certif-kosher-image", array( "alt" => "U", "proportional" => "true" ) ); ?><!--
+							--><h3 class="quality__title"><?php echo types_render_field( "section-certif-kosher-title", array( ) ); ?></h3>
+						</div><!--
+					--><div class="quality-certifications__content">
+							<p class="quality__certifications">
+							<?php echo types_render_field( "section-certif-kosher-content", array("output" => "raw" ) ); ?>
+							</p>
+						</div>
 					</div>
-				</div><!--
-			--><div class="quality-certifications__wrapper">
-					<div class="quality-certifications__pictitle title_dotted">
-						<img src="<?php bloginfo('template_directory'); ?>/img/logo-u.png" alt=""><!--
-						--><h3 class="quality__title">Certified Kosher</h3>
-					</div><!--
-				--><div class="quality-certifications__content">
-						<p class="quality__certifications">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh, ultricies in nisl eget, commodo tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh, ultricies in nisl eget, commodo tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit mauris a efficitur consequat. Sed ex nibh.
-						</p>
-					</div>
-				</div>
-			</section>
+				</section>
+
+			<?php endwhile; // End of the loop.?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
